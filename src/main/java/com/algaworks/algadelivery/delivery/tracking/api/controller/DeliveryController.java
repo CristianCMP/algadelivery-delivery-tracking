482a17a182be69b3739f0130aa6a2a967cd2c35f
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-25 15:10:50 -0300

    Endpoints for commands

--- src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
+++ src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algadelivery.delivery.tracking.api.controller;
 
+import com.algaworks.algadelivery.delivery.tracking.api.model.CourierIdInput;
 import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
 import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
 import com.algaworks.algadelivery.delivery.tracking.domain.repository.DeliveryRepository;
@@ -44,6 +45,22 @@ public class DeliveryController {
     public Delivery findById(@PathVariable UUID deliveryId) {
         return deliveryRepository.findById(deliveryId)
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/{deliveryId}/placement")
+    public void place(@PathVariable UUID deliveryId) {
+
+    }
+
+    @PostMapping("/{deliveryId}/pickups")
+    public void pickup(@PathVariable UUID deliveryId,
+                       @Valid @RequestBody CourierIdInput input) {
+
+    }
+
+    @PostMapping("/{deliveryId}/completion")
+    public void complete(@PathVariable UUID deliveryId) {
+
     }
 
 }
