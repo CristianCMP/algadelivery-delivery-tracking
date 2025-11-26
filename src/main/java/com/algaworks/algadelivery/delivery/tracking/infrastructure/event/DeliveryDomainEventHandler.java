ac6265f38913bbb8b81a4a41c352ea570e4f078f
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-25 21:31:39 -0300

    Listening to and reacting to Domain Events

--- src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
+++ src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/event/DeliveryDomainEventHandler.java
@@ -8,8 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class DeliveryDomainEventHandler {
 
