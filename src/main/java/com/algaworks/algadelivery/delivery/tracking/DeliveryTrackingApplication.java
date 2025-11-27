488676cf768a02bcbdbf66b2713714f71ffb1e13
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-27 17:12:04 -0300

    Integrating microservices into Eureka

--- src/main/java/com/algaworks/algadelivery/delivery/tracking/DeliveryTrackingApplication.java
+++ src/main/java/com/algaworks/algadelivery/delivery/tracking/DeliveryTrackingApplication.java
@@ -2,7 +2,9 @@ package com.algaworks.algadelivery.delivery.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DeliveryTrackingApplication {
 
