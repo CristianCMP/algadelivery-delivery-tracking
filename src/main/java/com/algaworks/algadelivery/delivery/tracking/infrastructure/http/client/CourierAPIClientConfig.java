0221d8461403cba2ebc496549ff2b75ae702b172
Cristian Puhl <cristian.puhl@universo.univates.br>
2025-11-28 18:59:27 -0300

    Configuring Timeout Pattern in Spring RestClient

--- src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ src/main/java/com/algaworks/algadelivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -3,6 +3,8 @@ package com.algaworks.algadelivery.delivery.tracking.infrastructure.http.client;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -19,9 +21,19 @@ public class CourierAPIClientConfig {
     @Bean
     public CourierAPIClient courierAPIClient(RestClient.Builder builder) {
 //        RestClient restClient = builder.baseUrl("http://localhost:8081").build();
-        RestClient restClient = builder.baseUrl("http://courier-management").build();
+        RestClient restClient = builder.baseUrl("http://courier-management")
+                .requestFactory(generateClientHttpRequestFactory())
+                .build();
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
         return proxyFactory.createClient(CourierAPIClient.class);
+    }
+
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(10);
+        factory.setReadTimeout(200);
+        return factory;
     }
 }
